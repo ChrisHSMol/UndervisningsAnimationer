cd99d4ae4cb39bfb67019b1ce6520f23ac1f568a
ChrisHSMol <christoffer@milinach.dk>
2024-03-26 15:24:51 +0100

    supermatematik ligningsløsning html

--- supermatematik/ligningsloesning.py
+++ supermatematik/ligningsloesning.py
@@ -22,6 +22,7 @@ _FRAMERATE = {
 
 
 class LigningsLoesning(Slide if slides else Scene):
+# class LigningsLoesning(Scene):
     btransparent = True
 
     def slide_pause(self, t=1.0, slides_bool=slides):
@@ -47,9 +48,9 @@ class LigningsLoesning(Slide if slides else Scene):
 
         forklaringer = VGroup(
             VGroup(
-                Tex("Mål: At samle alle ", "x", " på den ene side"),
+                Tex("Mål:  At samle alle ", "x", " på den ene side"),
                 Tex("     og alle ", "ikke-x", " på den anden side")
-            ).arrange(DOWN, aligned_edge=LEFT),
+            ).arrange(DOWN, aligned_edge=RIGHT),
             Tex("Ryk ", "$4x$", " over på venstre side"),
             Tex("Reducér"),
             Tex("Ryk ", "$-1$", " over på højre side"),
@@ -63,54 +64,160 @@ class LigningsLoesning(Slide if slides else Scene):
             forklaringer[i][1].set_color(RED)
 
         self.play(
-            Write(ligninger[0]),
-            Write(forklaringer[0], run_time=0.5)
+            # Write(ligninger[0]),
+            # Write(forklaringer[0][0], run_time=0.5)
+            FadeIn(ligninger[0]),
+            FadeIn(forklaringer[0][0])
+        )
+        self.play(
+            ligninger[0][0].animate.set_color(RED),
+            ligninger[0][4].animate.set_color(RED)
         )
         self.slide_pause()
+        self.play(
+            ligninger[0][0].animate.set_color(WHITE),
+            ligninger[0][4].animate.set_color(WHITE),
+            forklaringer[0][0][1].animate.set_color(WHITE),
+            # Write(forklaringer[0][1], run_time=0.5),
+            FadeIn(forklaringer[0][1]),
+            ligninger[0][2].animate.set_color(RED),
+            ligninger[0][6].animate.set_color(RED)
+        )
+        self.slide_pause()
+        self.play(
+            ligninger[0][2].animate.set_color(WHITE),
+            ligninger[0][6].animate.set_color(WHITE)
+        )
 
         i = 0
         self.play(
-            TransformMatchingTex(ligninger[i], ligninger[i+1], transform_mismatches=True),
-            TransformMatchingTex(forklaringer[i], forklaringer[1+i], transform_mismatches=True)
+            # TransformMatchingTex(ligninger[i], ligninger[i+1], transform_mismatches=True),
+            # TransformMatchingTex(forklaringer[i], forklaringer[1+i], transform_mismatches=True),
+            FadeOut(forklaringer[i], shift=0.5 * UP),
+            FadeIn(forklaringer[i+1], shift=0.5 * UP),
+            FadeIn(ligninger[i+1].shift(DOWN * 0.5), shift=UP * 0.5)
+        )
+        self.slide_pause()
+
+        self.play(
+            ligninger[i+1].animate.shift(0.5 * UP),
+            FadeOut(ligninger[i], shift=0.5 * UP)
         )
         self.slide_pause()
 
         i += 1
         self.play(
-            TransformMatchingTex(ligninger[i], ligninger[i+1], transform_mismatches=True),
-            TransformMatchingTex(forklaringer[i], forklaringer[1+i], transform_mismatches=True)
+            FadeOut(forklaringer[i], shift=0.5 * UP),
+            FadeIn(forklaringer[i+1], shift=0.5 * UP),
+            FadeIn(ligninger[i+1].shift(DOWN * 0.5), shift=UP * 0.5)
+        )
+        self.slide_pause()
+
+        self.play(
+            ligninger[i+1].animate.shift(0.5 * UP),
+            FadeOut(ligninger[i], shift=0.5 * UP)
         )
         self.slide_pause()
 
         i += 1
         self.play(
-            TransformMatchingTex(ligninger[i], ligninger[i+1], transform_mismatches=True),
-            TransformMatchingTex(forklaringer[i], forklaringer[1+i], transform_mismatches=True)
+            FadeOut(forklaringer[i], shift=0.5 * UP),
+            FadeIn(forklaringer[i+1], shift=0.5 * UP),
+            FadeIn(ligninger[i+1].shift(DOWN * 0.5), shift=UP * 0.5)
+        )
+        self.slide_pause()
+
+        self.play(
+            ligninger[i+1].animate.shift(0.5 * UP),
+            FadeOut(ligninger[i], shift=0.5 * UP)
         )
         self.slide_pause()
 
         i += 1
         self.play(
-            TransformMatchingTex(ligninger[i], ligninger[i+1], transform_mismatches=True),
-            TransformMatchingTex(forklaringer[i], forklaringer[1+i], transform_mismatches=True)
+            FadeOut(forklaringer[i], shift=0.5 * UP),
+            FadeIn(forklaringer[i+1], shift=0.5 * UP),
+            FadeIn(ligninger[i+1].shift(DOWN * 0.5), shift=UP * 0.5)
+        )
+        self.slide_pause()
+
+        self.play(
+            ligninger[i+1].animate.shift(0.5 * UP),
+            FadeOut(ligninger[i], shift=0.5 * UP)
         )
         self.slide_pause()
 
         i += 1
         self.play(
-            # TransformMatchingTex(ligninger[i], ligninger[i+1], transform_mismatches=True)
-            ReplacementTransform(ligninger[i], ligninger[i+1]),
-            TransformMatchingTex(forklaringer[i], forklaringer[1+i], transform_mismatches=True)
+            FadeOut(forklaringer[i], shift=0.5 * UP),
+            FadeIn(forklaringer[i+1], shift=0.5 * UP),
+            FadeIn(ligninger[i+1].shift(DOWN * 1.0), shift=UP * 0.5)
+        )
+        self.slide_pause()
+
+        self.play(
+            ligninger[i+1].animate.shift(1.0 * UP),
+            FadeOut(ligninger[i], shift=0.5 * UP)
         )
         self.slide_pause()
 
         i += 1
         self.play(
-            # TransformMatchingTex(ligninger[i], ligninger[i+1], transform_mismatches=True)
-            ReplacementTransform(ligninger[i], ligninger[i+1]),
-            TransformMatchingTex(forklaringer[i], forklaringer[1+i], transform_mismatches=True)
+            FadeOut(forklaringer[i], shift=0.5 * UP),
+            FadeIn(forklaringer[i+1], shift=0.5 * UP),
+            FadeIn(ligninger[i+1].shift(DOWN * 1.0), shift=UP * 0.5)
         )
         self.slide_pause()
+
+        self.play(
+            ligninger[i+1].animate.shift(1.0 * UP),
+            FadeOut(ligninger[i], shift=0.5 * UP)
+        )
+        self.slide_pause()
+
+        self.play(
+            LaggedStart(
+                *[FadeOut(m) for m in self.mobjects],
+                lag_ratio=0.25
+            )
+        )
+
+        # i += 1
+        # self.play(
+        #     TransformMatchingTex(ligninger[i], ligninger[i+1], transform_mismatches=True),
+        #     TransformMatchingTex(forklaringer[i], forklaringer[1+i], transform_mismatches=True)
+        # )
+        # self.slide_pause()
+        #
+        # i += 1
+        # self.play(
+        #     TransformMatchingTex(ligninger[i], ligninger[i+1], transform_mismatches=True),
+        #     TransformMatchingTex(forklaringer[i], forklaringer[1+i], transform_mismatches=True)
+        # )
+        # self.slide_pause()
+        #
+        # i += 1
+        # self.play(
+        #     TransformMatchingTex(ligninger[i], ligninger[i+1], transform_mismatches=True),
+        #     TransformMatchingTex(forklaringer[i], forklaringer[1+i], transform_mismatches=True)
+        # )
+        # self.slide_pause()
+        #
+        # i += 1
+        # self.play(
+        #     # TransformMatchingTex(ligninger[i], ligninger[i+1], transform_mismatches=True)
+        #     ReplacementTransform(ligninger[i], ligninger[i+1]),
+        #     TransformMatchingTex(forklaringer[i], forklaringer[1+i], transform_mismatches=True)
+        # )
+        # self.slide_pause()
+        #
+        # i += 1
+        # self.play(
+        #     # TransformMatchingTex(ligninger[i], ligninger[i+1], transform_mismatches=True)
+        #     ReplacementTransform(ligninger[i], ligninger[i+1]),
+        #     TransformMatchingTex(forklaringer[i], forklaringer[1+i], transform_mismatches=True)
+        # )
+        # self.slide_pause()
 
 
 if __name__ == "__main__":
